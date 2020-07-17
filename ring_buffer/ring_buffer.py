4e4b04fcc86fe8bc42adcdd8832543840922e092
mtoce <mtoce@villanova.edu>
2020-07-17 12:14:08 -0400

    completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.index = 0
+        self.storage = []
 
     def append(self, item):
-        pass
+        # if there is capacity left, append to storage
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        # begin overwrite existing elements
+        else:
+            self.storage[self.index] = item
+        self.index += 1
+        # if the index hits capacity, go back to index 0 and begin overwriting there
+        if self.index == self.capacity:
+            self.index = 0
 
     def get(self):
-        pass
+        return self.storage
